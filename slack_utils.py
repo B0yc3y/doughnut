483a0d17c681295074b1b202a9e922953e74d742
James Boyce <gitlab@its-jam.es>
2021-09-18 15:19:56 +1000

    Update slack usage, remove a number of calls and update text formatting

--- slack_utils.py
+++ slack_utils.py
@@ -3,67 +3,88 @@ from concurrent.futures import ThreadPoolExecutor
 from typing import List, Dict
 
 from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+from slack_sdk.models.blocks import Block
+from slack_sdk.web import SlackResponse
 
 SLACK_USER = '@doughnut-bot'
 
 
-def get_user_list(session, channel_id) -> List[dict]:
+def get_user_list(channel_id: str, session: WebClient, summary_only: bool) -> List[Dict]:
     """
     Fetch basic details for all active, non-bot users in this channel
-    :param session: a current Slack API session
     :param channel_id: Slack channel unique ID
+    :param session: a current Slack API session
+    :param summary_only: return only a summary of user data instead of all details
     :return: A list with an {id, name, real_name, timezone} entry for each active, non-bot user in this channel
     """
-    user_info_list = []
+    users: List[Dict] = get_channel_users(
+        channel_id=channel_id,
+        session=session,
+        active_users_only=True
+    )
 
-    response = session.conversations_members(channel=channel_id, limit=200)
-    user_list = response['members']
+    if summary_only:
+        # only get the summary fields needed for matching
+        users = [{
+            'id': user['id'],
+            'name': user['name'],
+            'real_name': user['real_name'],
+            'tz': user['tz'],
+            'tzOffset': user['tz_offset']
+        } for user in users]
 
-    user_detail_responses = get_all_user_data(user_list, session)
-
-    for resp in user_detail_responses:
-        user = resp['user']
-        if (user is not None
-                and not user['deleted']
-                and not user['is_restricted']
-                and not user['is_bot']
-                and 'donut' not in user['name']
-                and 'doughnut' not in user['name']):
-
-            user_info_list.append({
-                'id': user['id'],
-                'name': user['name'],
-                'real_name': user['real_name'],
-                'tz': user['tz']
-            })
-
-    if len(user_info_list) == 0:
+    if len(users) == 0:
         print(f"No Suitable users found in channel: {channel_id}")
 
-    return user_info_list
+    return users
 
 
 def get_user_wrapper(user, session):
     return session.users_info(user=user, include_locale=True)
 
 
-def get_all_user_data(users, session) -> List[Dict]:
-    user_details = []
-    with ThreadPoolExecutor() as executor:
-        running_tasks = [executor.submit(get_user_wrapper, user, session) for user in users]
-        for running_task in running_tasks:
-            result = running_task.result()
-            user_details.append(result)
-    return user_details
+def get_channel_users(channel_id: str, session: WebClient, active_users_only: bool) -> List[Dict]:
+    try:
+        # Get all ids of users in the channel
+        channel_users_response: SlackResponse = session.conversations_members(channel=channel_id)
+
+        # Get user details for all users in the slack team
+        team_users_response: SlackResponse = session.users_list()
+
+    except SlackApiError as e:
+        print(f"Error fetching data from Slack API: {e}")
+        raise SlackApiError
+
+    channel_user_ids: List[str] = channel_users_response['members']
+    slack_team_users: List[Dict] = team_users_response['members']
+
+    # todo add filtering here for match aversion/temporarily excluded users.
+    # filter out inactive users.
+    if active_users_only:
+        slack_team_users = [user for user in slack_team_users if is_user_active(user)]
+
+    # Return all the user details for users in the channel
+    return [user for user in slack_team_users if user["id"] in channel_user_ids]
 
 
-def create_match_dms(matches: List[dict], session: WebClient):
+def is_user_active(user: Dict) -> bool:
+    return (user is not None
+            and not user['deleted']
+            and not user['is_restricted']
+            and not user['is_bot']
+            and 'donut' not in user['name']
+            and 'doughnut' not in user['name'])
+
+
+def create_match_dms(matches: List[Dict], session: WebClient):
     with ThreadPoolExecutor() as executor:
         for match in matches:
             user1_id = match['user1']['id']
             user2_id = match['user2']['id']
             conversation_id_future = executor.submit(get_match_conversation_id, [user1_id, user2_id], session)
             executor.submit(create_match_dm, conversation_id_future.result(), user1_id, user2_id, session)
+            match["conv_id"] = conversation_id_future.result()
 
 
 def get_match_conversation_id(user_ids: List[str], session: WebClient) -> str:
@@ -71,48 +92,119 @@ def get_match_conversation_id(user_ids: List[str], session: WebClient) -> str:
     return response['channel']['id']
 
 
-def direct_message_match(user1_name: str, user2_name: str, user_id_lookup: Dict[str, str], message: str, session: WebClient):
+def direct_message_match(
+        user1_name: str,
+        user2_name: str,
+        user_id_lookup: Dict[str, str],
+        preview_message: str,
+        messages: [str],
+        session: WebClient
+) -> SlackResponse:
     user1_id = user_id_lookup[user1_name]
     user2_id = user_id_lookup[user2_name]
     conv_id = get_match_conversation_id([user1_id, user2_id], session)
-    session.chat_postMessage(
+
+    return session.chat_postMessage(
         channel=conv_id,
-        as_user=SLACK_USER,
-        text=message
+        text=preview_message,
+        blocks=Block.parse_all([
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": message
+                }
+            } for message in messages
+        ]),
     )
 
 
-def create_match_dm(conv_id: str, user1_id: str, user2_id: str, session: WebClient):
+def create_match_dm(conv_id: str, user1_id: str, user2_id: str, session: WebClient) -> str:
     ids: List[str] = [user1_id, user2_id]
+    organiser_id: str = ids[random.randint(0, 1)]
 
-    organiser = ids[random.randint(0, 1)]
+    response: SlackResponse = session.chat_postMessage(
+        channel=conv_id,
+        text=f':doughnut: doughnut Time! :doughnut: ',
+        blocks=Block.parse_all(
+            [{
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": f'Hello <@{user1_id}> and <@{user2_id}>!'
+                            f' Welcome to a new round of doughnuts!'
+                            f' Please use this DM channel to set up time to connect!'
+                            f'<@{organiser_id}> you have been selected to organise the meeting.'
+                }
+            }]
+        ),
+    )
 
-    session.chat_postMessage(channel=conv_id,
-                             text=f'Hello <@{user1_id}> and <@{user2_id}>! Welcome to a new round of doughnuts! '
-                                  f'Please use this DM channel to set up time to connect!',
-                             as_user=SLACK_USER)
-    session.chat_postMessage(channel=conv_id,
-                             text=f'<@{organiser}> you have been selected to organise the meeting.',
-                             as_user=SLACK_USER)
+    if not response.status_code == 200:
+        print(f"Unable to message {user1_id} & {user2_id}")
+
+    return conv_id
 
 
-def post_matches(session: WebClient, matches: List[dict], my_channel_id: str):
+def post_matches(session: WebClient, matches: List[dict], my_channel_id: str) -> SlackResponse:
     """
     Creates a new DM for each pair of users to introduce them,
     and also posts a list of all pairings to the channel
     """
     create_match_dms(matches, session)
+    preview_message: str = ":doughnut: Matches are in! :doughnut:"
 
-    message: str = 'The new round of pairings are in! You should have received a DM from _doughnut with your new ' \
-                   'doughnut partner. If there are an odd number of participants, ' \
-                   'someone will get two matches.\n' \
-                   'Please post any issues to https://github.com/B0yc3y/doughnut/issues'
+    match_message: str = "The matches for this round:"
     for match in matches:
         user1_id = match['user1']['id']
         user2_id = match['user2']['id']
-        message += f'\n<@{user1_id}> and <@{user2_id}>'
+        match_message += f'\n<@{user1_id}> and <@{user2_id}>'
 
-    message += f"\nThat's {len(matches)} donuts this time around!"
+    blocks: List[Block] = Block.parse_all([
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": 'The new round of pairings are in!\n'
+                        'You should have received a DM from _doughnut with your new doughnut partner.\n'
+                        'If there are an odd number of participants, someone will get two matches.'
+            }
+        },
+        {
+            "type": "divider"
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": 'Please post any issues to https://github.com/B0yc3y/doughnut/issues'
+            }
+        },
+        {
+            "type": "divider"
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": match_message
+            }
+        },
+        {
+            "type": "divider"
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": f"\nThat's {len(matches)} doughnut(s) this time around!"
+            }
+        }
+    ])
 
-    # Send pairings to the ds_donut channel
-    session.chat_postMessage(channel=my_channel_id, text=message, as_user='@doughnut_bot')
+    # Send pairings to the ds_doughnut channel
+    return session.chat_postMessage(
+        channel=my_channel_id,
+        text=preview_message,
+        blocks=blocks
+    )
